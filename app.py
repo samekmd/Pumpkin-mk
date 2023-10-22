9e428fdc037ebcd3e36ef45dd1cb63b63da534d6
samekmd <samuelalkmc@gmail.com>
2023-10-22 17:09:03 -0300

    UPADTE MENU
    Começo do CSS do menu principal

--- app.py
+++ app.py
@@ -5,3 +5,16 @@ app = Flask('__name__')
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/Comece')
+def comece():
+    return render_template('comece.html')
+
+@app.route('/Consultoria')
+def consultoria():
+    return render_template('consultoria.html')
+
+@app.route('/Login')
+def login():
+    return render_template('login.html')
+
