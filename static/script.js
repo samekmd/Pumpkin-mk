59e8982e4e9b457e9fd6844a3819db64fd7dbe99
samekmd <samuelalkmc@gmail.com>
2023-10-25 20:30:13 -0300

    UPDATE Index
    Cria Carousel

--- static/script.js
+++ static/script.js
@@ -1,7 +1,29 @@
-alert("The page are reloading")
+alert("The page is reloading")
+
+//Inicop do Carousel 
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById("radio1").checked = true
 
 
+//setInterval(() => {
+   // proximaImg()
+//}, 5000)
 
+function  proximaImg(){
+     cont++
+
+     if(cont > 3){
+        cont = 1
+     }
+
+     document.getElementById("radio"+cont).checked = true
+
+}
+
+
+//Fim do Carousel
  
 
 
