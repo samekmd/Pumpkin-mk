0c67e1a3d94f09b61debb5a784e5b8048c880c34
samekmd <samuelalkmc@gmail.com>
2023-11-10 16:46:06 -0300

    Update Login/ app.py
    Inicio da implementação de banco de dados.

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, jsonify
+
+# from flask_mysqldb import MySQL  
+
+# app.config['MYSQL_Host'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = '1938'
+# app.config['MYSQL_DB'] = 'login'
+
+# mysql = MySQL(app)
+
 
 app = Flask('__name__')
 
@@ -14,7 +24,19 @@ def comece():
 def consultoria():
     return render_template('comece.html')
 
-@app.route('/Login')
+@app.route('/Login', methods=('GET', 'POST'))
 def login():
+    # if request.method == 'POST': 
+    #         nome = request.method['nome']
+    #         senha = request.method['senha']
+    #         cur = mysql.connection.cursor()
+    #         cur.execute("INSERT INTO login(nome,senha), VALUES (%s, %s)",(nome,senha))
+
+    #         mysql.connection.commit()
+            
+    #         cur.close()
+             
+    #         return 'Dados Salvos com Sucesso!'
+        
     return render_template('login.html')
 
