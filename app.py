a83155c6af8f50a40f41e3c3bcb0a1d034c04ecc
samekmd <samuelalkmc@gmail.com>
2023-11-22 10:34:14 -0300

    UPDATE USERS
    Listagem do banco de dados na página funcionando.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_mysqldb import MySQL 
+
 # from flask_login import LoginManager
 # from flask_sqlalchemy import SQLAlchemy
 
